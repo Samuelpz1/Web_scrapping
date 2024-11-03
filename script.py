9785d616cccb58877f8d7abaea381dbb18834042
Samuel Padierna <samuelpz9912@gmail.com>
2024-11-02 19:35:09 -0500

    Final version to complete the buying process

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 USER = "standard_user"
-PASSWORD = "standard_user"
+PASSWORD = "secret_sauce"
 
 def main():
     service = Service(ChromeDriverManager().install())
@@ -16,7 +16,25 @@ def main():
     option.add_argument("--window-size=1920,1800")
     driver = Chrome(service=service,options=option)
     driver.get("https://www.saucedemo.com/")
-    time.sleep(5)
+
+    #LOGIN
+    driver.find_element(By.ID, "user-name").send_keys(USER)
+    driver.find_element(By.ID, "password").send_keys(PASSWORD)
+    driver.find_element(By.ID, "login-button").click()
+    
+    #BUY
+    driver.find_element(By.NAME, 'add-to-cart-sauce-labs-bike-light').click()
+    driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
+    driver.find_element(By.XPATH,"/html/body/div/div/div/div[1]/div[1]/div[3]/a").click()
+
+    #CHECKOUT
+    driver.find_element(By.ID,'checkout').click()
+    driver.find_element(By.ID, "first-name").send_keys('Name')
+    driver.find_element(By.ID, "last-name").send_keys('Last Name')
+    driver.find_element(By.ID, "postal-code").send_keys('0000')
+    driver.find_element(By.ID, "continue").click()
+    driver.find_element(By.ID, "finish").click()
+    time.sleep(10)
     driver.quit()
     
 
